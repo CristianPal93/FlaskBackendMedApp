caa5f0875332b5a5543367b3fa1ce160166e13e6
cristian <palcristi01@gmail.com>
2020-12-06 22:29:56 +0200

    Flask Backend

---
+++ dataBase.py
@@ -0,0 +1,138 @@
+import sqlite3
+import sqlalchemy
+from sqlalchemy import create_engine
+from datetime import datetime
+import os.path
+from os import path
+
+
+class DataBase:
+    def __init__(self):
+        self.connection = None
+        try:
+            self.__create_dataBase()
+        except sqlalchemy.exc.OperationalError:
+            print("Table exists in the database return a connection to the schema instead!")
+            self.connection=self.__create_connection()
+
+    def __get_curent_time(self):
+        now = datetime.now()
+        dt_string = now.strftime("%Y-%m-%d %H:%M:%S")
+        return dt_string
+
+    def __create_dataBase(self):
+        engine = create_engine('sqlite:///schema.db', echo=True)
+        self.connection=engine.connect()
+        sql_statement='CREATE TABLE Person(LastName varchar(255) NOT NULL ,FirstName varchar(255) NOT NULL,Cnp varchar(13) primary key ,Job varchar(20),Email varchar(255) not NULL ,Password VARCHAR (20) not NULL );'
+        self.connection.execute(sql_statement)
+        sql_statement='CREATE TABLE Specialization(Cnp varchar(13),Specialization varchar(255),phoneNumber varchar(12) , FOREIGN KEY (Cnp) REFERENCES Person(Cnp));'
+        self.connection.execute(sql_statement)
+        sql_statement = 'CREATE TABLE Schedule(Cnp varchar(13),FirstName varchar(255), LastName varchar(255),FirstNameDoctor varchar(255), LastNameDoctor varchar(255),Specialization varchar(255),ScheduleTime TIMESTAMP UNIQUE , FOREIGN KEY (Cnp) REFERENCES Person(Cnp));'
+        self.connection.execute(sql_statement)
+
+    def __create_connection(self):
+        engine = create_engine('sqlite:///schema.db', echo=True)
+        conn=engine.connect()
+        return conn
+
+    def wirite_to_db(self, query):
+        if path.exists('schema.db'):
+            print('Schema is present!')
+            try:
+                if self.connection is not None:
+                    self.connection.execute(query)
+                else:
+                    self.connection=self.__create_connection()
+                    self.connection.execute(query)
+            except sqlalchemy.exc.IntegrityError or sqlite3.IntegrityError:
+                   print("This record exists in the table!")
+        else:
+            print('Schema is missing!')
+            self.__create_dataBase()
+            self.wirite_to_db(query)
+
+    def write_schedule(self, pacient_cnp, doctor_first_name, doctor_last_name, doctor_specialization):
+        if (path.exists('schema.db')):
+            print('Schema is present!')
+            pacient = self.get_from_db('Select * from person where cnp='+pacient_cnp+';')
+            doctor = self.get_from_db('Select * from person where LastName="'+doctor_last_name+'" and FirstName="' + doctor_first_name+'";')
+            spec = self.get_from_db('Select * from Specialization where cnp="'+doctor[0][3]+'" and Specialization="' + doctor_specialization+'";')
+            print(spec)
+            print(doctor)
+            sql = 'Insert into Schedule(Cnp,FirstName,LastName,FirstNameDoctor,LastNameDoctor,Specialization,ScheduleTime) VALUES ("'+pacient[0][3]+'","'+pacient[0][0]+'","'+pacient[0][1]+'","'+doctor[0][0]+'","'+doctor[0][1]+'","'+spec[0][1]+'","'+self.__get_curent_time()+'");'
+            self.connection.connect()
+            self.connection.execute(sql)
+        else:
+            print('Schema is missing!')
+            self.__create_dataBase()
+            self.write_schedule(self, pacient_cnp, doctor_first_name, doctor_last_name, doctor_specialization)
+
+    def get_from_db(self, query):
+        if self.connection is not None:
+            self.connection.connect()
+            result = self.connection.execute(query)
+            result = result.fetchall()
+        else:
+            self.connetion = self.__create_connection()
+            result = self.connection.execute(query)
+            result = result.fetchall()
+        return result
+
+    def close_connetion(self):
+        self.connection.close()
+
+    def open_connetion(self):
+        self.connection.connect()
+
+    def checkUser(self,username,password):
+        sqlQuery="SELECT * FROM PERSON WHERE email='"+username+"' and Password='"+password+"';"
+        result=self.get_from_db(sqlQuery)
+        if len(result)>0:
+            return True
+        else:
+            return False
+    def checkJob(self,username,password):
+        sqlQuery = "SELECT * FROM PERSON WHERE email='" + username + "' and Password='" + password + "';"
+        result = self.get_from_db(sqlQuery)
+        if len(result)>0:
+            return result[0][3]
+        return None
+
+    def insertPacient(self,lastName,firstName,cnp,job,email,password):
+        sql = "Insert INTO Person(LastName,FirstName,Cnp,Job,Email,Password) VALUES ('"+firstName+"','"+lastName+"','"+cnp+"','"+job+"','"+email+"','"+password+"');"
+        self.wirite_to_db(sql)
+    def get_staff(self,job):
+        sql = "Select * from Person where job='"+job+"';"
+        return self.get_from_db(sql)
+    def doctor_specializations(self,cnp):
+        sql = "Select * from Specialization where cnp='"+cnp+"';"
+        return self.get_from_db(sql)
+if __name__ == "__main__":
+    d = DataBase()
+    print(d.doctor_specializations('1910924403932'))
+    # rez=d.checkUser("cristian.pal@gmail.com","Admin123")
+    # print(rez)
+    # print(d.checkJob("cristian.pal@gmail.com","Admin123"))
+
+    # sql = 'Insert INTO Person(LastName,FirstName,Cnp,Job,Email,Password) VALUES ("Cristian","Pal","1930925303932","pacient","cristian.pal@gmail.com","Admin123");'
+    # d.wirite_to_db(sql)
+    # # sql = 'SELECT * FROM person where email="cristian.pal@gmail.com" and password="Admin123"';
+    # # print(d.get_from_db(sql))
+    # sql = 'Insert INTO Person(LastName,FirstName,Cnp,Job,Email,password) VALUES ("Laurentiu","Girleanu","1910925303932","doctor","laurentiu.girleanu@gmail.com","Admin1234");'
+    # d.wirite_to_db(sql)
+    # sql = 'Insert INTO Person(LastName,FirstName,Cnp,Job,Email,password) VALUES ("Ion","Popescu","1910924403932","doctor","ion.popescu@gmail.com","Admin12345");'
+    # d.wirite_to_db(sql)
+    # sql = 'SELECT * FROM person';
+    # # print(d.get_from_db(sql))
+    # sql='Insert INTO Specialization(Cnp,Specialization,phoneNumber) VALUES ("1910925303932","Psiholog","+40742451132");'
+    # d.wirite_to_db(sql)
+    # sql = 'Insert INTO Specialization(Cnp,Specialization,phoneNumber) VALUES ("1910924403932","Neurolog","+40742451123");'
+    # d.wirite_to_db(sql)
+    # sql = 'Delete FROM specialization where cnp="1910924403932";'
+    # d.wirite_to_db(sql)
+    # d.write_schedule('1930925303932', "Girleanu", "Laurentiu", "Chirurg")
+    # sql = 'Select * from Schedule;'
+    # print(d.get_from_db(sql))
+
+
+
---
+++ main.py
@@ -0,0 +1,117 @@
+import datetime
+from functools import wraps
+import jwt
+from jwt import ExpiredSignatureError
+from flask import Flask, request, jsonify, make_response
+from flask_cors import CORS
+
+from dataBase import DataBase
+
+app = Flask(__name__)
+
+app.config['SECRET_KEY'] = b'_5#y2L"F4Q8z\n\xec]/'
+app.config['Access-Control-Allow-Origin'] = '*'
+CORS(app)
+
+def get_list_of_spec(l):
+    l_spec=[]
+    l_phone=l[0][2]
+    for i in l:
+        l_spec.append(i[1])
+    return l_spec,l_phone
+def token_required(f):
+    @wraps(f)
+    def decorated(*args, **kwargs):
+        token = None
+        if 'x-access-token' in request.headers:
+            token = request.headers['x-access-token']
+        if not token:
+            return jsonify({'message': 'Token is missing!'}), 401
+        try:
+            data = jwt.decode(token, app.config['SECRET_KEY'])
+            current_user = data['USER_KEY']
+        except ExpiredSignatureError:
+            return make_response({'message': 'Expired token, please login again'}), 401
+        except:
+            return jsonify({'message': 'Token is invalid!'}), 401
+        return f(current_user, *args, **kwargs)
+
+    return decorated
+
+@app.route('/get-all-staff',methods=['POST'])
+def get_staff():
+    if "staff" in request.headers:
+        personal=request.headers['staff']
+        dataBase=DataBase()
+        rez=dataBase.get_staff(personal)
+        counter=0
+        doctor_list=[]
+        doctor={}
+        for i in rez:
+            specialization=dataBase.doctor_specializations(i[2])
+            l_spec,l_phone=get_list_of_spec(specialization)
+            doctor[personal+str(counter)]={'firstName':i[0],'lastName':i[1],'cnp':i[2],'email':i[4],'specializations':l_spec,'phone':l_phone}
+            counter+=1
+            doctor_list.append(doctor)
+            doctor={}
+        print(doctor_list)
+        return jsonify({personal:doctor_list})
+
+@app.route('/contact', methods=['POST'])
+def contact():
+    if "firstname" and "lastname" and "email"  and "subject" in request.headers:
+        return jsonify({"message": "Thank you for you message"})
+    return jsonify({"message": "Invalid request"})
+
+
+@app.route('/register-client',methods=['POST'])
+def register_client():
+    if "fullname" and "email" and "password" and "cnp" in request.headers:
+        fullname = request.headers['fullname']
+        email = request.headers['email']
+        password = request.headers['password']
+        cnp = request.headers['cnp']
+        dataBase=DataBase()
+        if dataBase.checkJob(email,password):
+            return jsonify({'message':"email already exists"})
+        firstName,lastName=fullname.split(" ")
+        job='pacient'
+        dataBase.insertPacient(lastName,firstName,cnp,job,email,password)
+        if dataBase.checkUser(email, password):
+            return jsonify({"message":"account created succesfully!"})
+        return jsonify({'message':'something went wrong.. :('})
+
+@app.route('/login', methods=['POST'])
+def login():
+    if "username" and "password" in request.headers:
+        username = request.headers['username']
+        password = request.headers['password']
+        print(username,password)
+        dataBase = DataBase()
+        tokenValability = datetime.datetime.utcnow() + datetime.timedelta(minutes=30)
+        """
+        check for user in database
+        """
+        if not dataBase.checkUser(username, password):
+            print('no user')
+            return make_response({'message': 'Invalid username or password'}, 401)
+        """
+        check for user rights in datebase
+        """
+        user_rights=dataBase.checkJob(username, password)
+        """
+        if everything is true
+        then
+        """
+        first_name_last_name=username.split("@")[0].split(".")
+        first_name=first_name_last_name[0]
+        last_name=first_name_last_name[1]
+        full_Name=first_name+" "+last_name
+        token = jwt.encode({'USER_KEY': username, 'exp': tokenValability},
+                           app.config['SECRET_KEY'])
+        return jsonify({'accessToken': token.decode('UTF-8'), 'USER_KEY': full_Name})
+    return make_response({'message': 'Invalid credentials'}, 401)
+
+
+if __name__ == "__main__":
+  app.run(debug=True)
